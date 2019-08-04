09aacf0ca9290e0c1321a693eba5da32048375ba
d954mas <d954mas@mail.ru>
2019-08-04 18:29:09 +0300

    Гуй

--- core/src/com/d954mas/game/indicator2019/speech/model/enemies/Enemies.java
+++ core/src/com/d954mas/game/indicator2019/speech/model/enemies/Enemies.java
@@ -8,7 +8,7 @@ public class Enemies {
     public static List<Enemy> enemyList;
     public static void init(){
         enemyList = Cs.of(
-                new BatEnemy(20,7,19,5,7),
+                new BatEnemy(20,7,9,5,7),
                 new RedGhostEnemy(30,10,12,6,8),
                 new SlugEnemy(30,12,15,4,8),
                 new SpudiEnemy(18,12,15,6,8),
